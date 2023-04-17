eed54f68ac395d1621644ab0dde0d3cfcaefc0a1
leo82380 <tkdrb062@gmail.com>
2023-04-17 18:46:23 +0900

    예외처리

--- Adofai_Level_recommend/Program.cs
+++ Adofai_Level_recommend/Program.cs
@@ -38,69 +38,67 @@ namespace AdofaiRandom
 
                         #region 난이도 설정1
                         //난이도 설정
-                        Console.WriteLine("최소 난이도를 입력하세요(소숫점 없이) : ");
-                        int minLevel = int.Parse(Console.ReadLine());
-                        if (minLevel <= 0 || minLevel % 1 != 0)
+                        try
                         {
-                            Console.WriteLine("잘못된 값을 입력하였습니다.");
-                            break;
-                        }
-                        
-                        Console.WriteLine("최대 난이도를 입력하세요(소숫점 없이) : ");
-                        int maxLevel = int.Parse(Console.ReadLine());
-                        if(maxLevel >= 22 || maxLevel % 1 != 0)
-                        {
-                            Console.WriteLine("잘못된 값을 입력하였습니다.");
-                            break;
-                        }
-                        if(minLevel > maxLevel)
-                        {
-                            Console.WriteLine("최소 난이도가 최대 난이도보다 큽니다.");
-                            Console.WriteLine("값을 교환합니다.");
-                            int change = minLevel;
-                            minLevel = maxLevel;
-                            maxLevel = change;
-                        }
-                        if(maxLevel >= 22)
-                        {
-                            Console.WriteLine("잘못된 값을 입력하였습니다.");
-                            break;
-                        }
-                        #endregion
+                            Console.WriteLine("최소 난이도를 입력하세요(소숫점 없이) : ");
+                            int minLevel = int.Parse(Console.ReadLine());
 
-                        #region 레벨 개수
-                        //몇번째 레벨
-                        Console.WriteLine("몇번째 레벨까지 추천합니까?");
-                        int curLevel = int.Parse(Console.ReadLine());
-                        if (curLevel <= 0)
-                        {
+                            Console.WriteLine("최대 난이도를 입력하세요(소숫점 없이) : ");
+                            int maxLevel = int.Parse(Console.ReadLine());
+
+                            if (minLevel > maxLevel)
+                            {
+                                Console.WriteLine("최소 난이도가 최대 난이도보다 큽니다.");
+                                Console.WriteLine("값을 교환합니다.");
+                                int change = minLevel;
+                                minLevel = maxLevel;
+                                maxLevel = change;
+                            }
+                            if (maxLevel >= 22)
+                            {
+                                Console.WriteLine("잘못된 값을 입력하였습니다.");
+                                break;
+                            }
+                            #endregion
+
+                            #region 레벨 개수
+                            //몇번째 레벨
+                            Console.WriteLine("몇번째 레벨까지 추천합니까?");
+                            int curLevel = int.Parse(Console.ReadLine());
+                            if (curLevel <= 0)
+                            {
+                                Console.WriteLine("잘못된 값을 입력하였습니다.");
+                                break;
+                            }
+
+                            //개수 추천
+                            Console.WriteLine("추천 레벨의 개수를 입력하세요");
+                            int level = int.Parse(Console.ReadLine());
+                            if (level <= 0)
+                            {
+                                Console.WriteLine("잘못된 값을 입력하였습니다.");
+                                break;
+                            }
+                            #endregion
+
+                            #region 레벨 추천 출력
+                            //여러개 추천하는 코드
+                            for (int i = 0; i < level; i++)
+                            {
+                                float b = a.Next(minLevel, maxLevel);
+                                Console.WriteLine($"난이도 : {b} ");
+
+                                int c = a.Next(1, curLevel);
+                                Console.WriteLine($"{c}번째 레벨");
+
+                                Console.WriteLine("");
+                            }
+                            #endregion
+                        }
+                        catch (FormatException e) {
+                            Console.WriteLine(e.Message);
                             Console.WriteLine("잘못된 값을 입력하였습니다.");
-                            break;
                         }
-                        
-                        //개수 추천
-                        Console.WriteLine("추천 레벨의 개수를 입력하세요");
-                        int level = int.Parse(Console.ReadLine());
-                        if (level <= 0)
-                        {
-                            Console.WriteLine("잘못된 값을 입력하였습니다.");
-                            break;
-                        }
-                        #endregion
-
-                        #region 레벨 추천 출력
-                        //여러개 추천하는 코드
-                        for (int i = 0; i < level; i++)
-                        {
-                            float b = a.Next(minLevel, maxLevel);
-                            Console.WriteLine($"난이도 : {b} ");
-
-                            int c = a.Next(1, curLevel);
-                            Console.WriteLine($"{c}번째 레벨");
-
-                            Console.WriteLine("");
-                        }
-                        #endregion
                         break;
                     #endregion
 
@@ -120,26 +118,34 @@ namespace AdofaiRandom
 
                         #region 개수 입력
                         //레벨 개수
-                        Console.WriteLine("추천 레벨 개수 입력(1~10개)");
-                        int levels = int.Parse(Console.ReadLine());
-                        if(levels <= 0 || levels >= 11)
+                        try
                         {
-                            Console.WriteLine("잘못된 값을 입력하였습니다.");
-                            break;
-                        }
-                        #endregion
+                            Console.WriteLine("추천 레벨 개수 입력(1~10개)");
+                            int levels = int.Parse(Console.ReadLine());
+                            if (levels <= 0 || levels >= 11)
+                            {
+                                Console.WriteLine("잘못된 값을 입력하였습니다.");
+                                break;
+                            }
+                            #endregion
 
-                        #region 레벨 출력
-                        //레벨 출력
-                        for (int i = 1; i <= levels; i++)
-                        {
-                            int levelcur = aaa.Next(0, 5000);
-                            Console.WriteLine($"추천레벨 {i} : {levelcur}");
-                            Console.WriteLine("레벨 링크 : ");
-                            Console.WriteLine($"adofai.gg/levels/{levelcur}");
-                            Console.WriteLine("");
+                            #region 레벨 출력
+                            //레벨 출력
+                            for (int i = 1; i <= levels; i++)
+                            {
+                                int levelcur = aaa.Next(0, 5000);
+                                Console.WriteLine($"추천레벨 {i} : {levelcur}");
+                                Console.WriteLine("레벨 링크 : ");
+                                Console.WriteLine($"adofai.gg/levels/{levelcur}");
+                                Console.WriteLine("");
+                            }
+                            #endregion
                         }
-                        #endregion
+                        catch(FormatException e)
+                        {
+                            Console.WriteLine(e.Message);
+                            Console.WriteLine("잘못된 값을 입력하였습니다.");
+                        }
                         break;
                     #endregion
 
